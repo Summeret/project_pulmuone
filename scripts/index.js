a46316640b01c269ac1dc7d6fb0b0d11641b7ff6
jiyun <jyiun27@gmail.com>
2025-07-04 18:12:22 +0900

    숏츠 진행중

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const newSwiper = new Swiper('.new_swiper', {
     slidesPerView: 1, // 슬라이드에 하나의 UL만 보여짐 (ul 내부에 상품 4개)
-    autoplay:{delay:3000},
+    //autoplay:{delay:3000},
     loop: true,
     navigation: {
         nextEl: '.swiper-button-next',
@@ -8,12 +8,20 @@ const newSwiper = new Swiper('.new_swiper', {
     },
     pagination: {
         el: '.swiper-pagination',
-        clickable: true,
+        type:'bullets'
     },
 });
+
+const olgaSwiper = new Swiper('.olga_swiper', {
+    slidesPerView:3.2, //전체개수가 아니라 보이는 개수가 기준
+    spaceBetween:10, //사이 여백
+    autoplay:{delay:2000},
+    loop:true,
+});
+
 const bestSwiper = new Swiper('.best_swiper', {
     slidesPerView: 1, // 슬라이드에 하나의 UL만 보여짐 (ul 내부에 상품 4개)
-    autoplay:{delay:3000},
+    //autoplay:{delay:3000},
     loop: true,
     navigation: {
         nextEl: '.swiper-button-next',
@@ -21,12 +29,41 @@ const bestSwiper = new Swiper('.best_swiper', {
     },
     pagination: {
         el: '.swiper-pagination',
-        clickable: true,
+        type:'bullets'
     },
 });
-const olgaSwiper = new Swiper('.olga_swiper', {
-    slidesPerView:4, //전체개수가 아니라 보이는 개수가 기준
+
+const mealSwiper = new Swiper('.meal_swiper', {
+    slidesPerView:3.1, //전체개수가 아니라 보이는 개수가 기준
     spaceBetween:10, //사이 여백
-    /* autoplay:{delay:2000},
-    loop:true, */
+    autoplay:{delay:2000},
+    loop:true,
+});
+
+const recommenSwiper = new Swiper('.recommen_swiper', {
+    slidesPerView: 1, // 슬라이드에 하나의 UL만 보여짐 (ul 내부에 상품 4개)
+    //autoplay:{delay:3000},
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type:'bullets'
+    },
+});
+
+const shortSwiper = new Swiper('.short_swiper', {
+    slidesPerView: 1, // 슬라이드에 하나의 UL만 보여짐 (ul 내부에 상품 4개)
+    //autoplay:{delay:3000},
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type:'bullets'
+    },
 });
