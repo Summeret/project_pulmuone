e411d376e7bfdeb4f93285fd273b2645dae37bca
jiyun <jyiun27@gmail.com>
2025-07-06 13:57:34 +0900

    풀무원 완성

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
+const adList = new Swiper('.ad_swiper',{
+    slidesPerView:1, //전체개수가 아니라 보이는 개수가 기준
+    spaceBetween:10, //사이 여백
+    autoplay:{delay:3000},
+    loop:true,
+    pagination: {
+        el: '.swiper-pagination',
+        type:'fraction'
+
+    },
+});
+
 const newSwiper = new Swiper('.new_swiper', {
     slidesPerView: 1, // 슬라이드에 하나의 UL만 보여짐 (ul 내부에 상품 4개)
     //autoplay:{delay:3000},
@@ -38,6 +50,9 @@ const mealSwiper = new Swiper('.meal_swiper', {
     spaceBetween:10, //사이 여백
     autoplay:{delay:2000},
     loop:true,
+    scrollbar: {
+	el: '.swiper-scrollbar',
+    },
 });
 
 const recommenSwiper = new Swiper('.recommen_swiper', {
@@ -54,16 +69,9 @@ const recommenSwiper = new Swiper('.recommen_swiper', {
     },
 });
 
-const shortSwiper = new Swiper('.short_swiper', {
-    slidesPerView: 1, // 슬라이드에 하나의 UL만 보여짐 (ul 내부에 상품 4개)
-    //autoplay:{delay:3000},
+const shortSwiper = new Swiper('.shorts_swiper', {
+    slidesPerView: 5.7, 
+    spaceBetween:0,
+    autoplay:{delay:3000},
     loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        type:'bullets'
-    },
 });
